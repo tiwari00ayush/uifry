2356dcce3ccb27411e529a744d1cf89dd2a81b28
Ayush Tiwari <107319179+tiwari00ayush@users.noreply.github.com>
2024-06-04 19:08:33 +0530

    blur problem solved

--- src/components/GradientBg.jsx
+++ src/components/GradientBg.jsx
@@ -3,18 +3,18 @@ import overlay from "../assets/overlay.jpeg";
 const GradientBg = ({ width = 500, height = 500, scale = 1 }) => {
   return (
     <div
-      className=" bg-white overflow-hidden "
+      className="bg-white overflow-hidden"
       style={{
         width: `${width}px`,
         height: `${height}px`,
-        scale: `${scale}`,
+        transform: `scale(${scale})`,
       }}
     >
       <div className="relative w-full h-full top-0 left-0">
         <div
-          className="absolute top-[150px] left-[60px] bg-[#ffed93] rounded-full"
+          className="absolute bg-[#ffed93] rounded-full"
           style={{
-            filter: "blur(351.3px)",
+            filter: "blur(35px)",
             width: `${(width * 2) / 3}px`,
             height: `${height / 2}px`,
             top: `${height / 3}px`,
@@ -22,16 +22,15 @@ const GradientBg = ({ width = 500, height = 500, scale = 1 }) => {
           }}
         ></div>
         <div
-          className="absolute w-[300px] h-[201px] top-[80px] left-[100px] bg-[#ff615e] rounded-full"
+          className="absolute bg-[#ff615e] rounded-full"
           style={{
-            filter: "blur(364.9px)",
+            filter: "blur(35px)",
             width: `${width / 1.667}px`,
             height: `${height / 2.5}px`,
             top: `${height / 6.25}px`,
             left: `${width / 5}px`,
           }}
         ></div>
-        {/* <div className="absolute w-[1802px] h-[1477px] top-0 left-0 shadow-lg"></div> */}
         <img
           className="absolute w-full h-full mix-blend-overlay"
           src={"https://momentsingraphics.de/Media/BlueNoise/WhiteNoiseFFT.png"}
